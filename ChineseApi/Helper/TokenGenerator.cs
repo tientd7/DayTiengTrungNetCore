6f28858d9bdf38e902b2aec240060bbdc39bf8d6
Tiến TD <tientd1302@outlook.com>
2019-12-25 23:11:19 +0700

    Update EXP token to 1 Month

--- ChineseApi/Helper/TokenGenerator.cs
+++ ChineseApi/Helper/TokenGenerator.cs
@@ -38,7 +38,7 @@ namespace ChineseApi.Helper
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity( claims.ToArray() ),
-                Expires = DateTime.UtcNow.AddDays(7),
+                Expires = DateTime.UtcNow.AddDays(30),
                 //Claims = tokenClaims,
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
